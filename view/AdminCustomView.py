dda66ca07521b79a06f3de33f8a845cd9fd25a8a
Aviv Laufer <aviv.laufer@gmail.com>
2018-02-02 17:08:20 +0200

    Clean up View
    Thanks @doodyparizada

--- view/AdminCustomView.py
+++ view/AdminCustomView.py
@@ -1,92 +1,104 @@
 """Admin view """
 import flask_admin
 from flask_admin.contrib import appengine
-from wtforms import validators, Form
-
+from wtforms import validators
+from collections import OrderedDict
 from validators import GreaterEqualThan, SmallerEqualThan
+
+
+class LastUpdatedOrderedDict(OrderedDict):
+    """Store items in the order the keys were last added"""
+
+    def __setitem__(self, key, value):
+        if key in self:
+            del self[key]
+        OrderedDict.__setitem__(self, key, value)
 
 
 class AdminCustomView(flask_admin.contrib.appengine.view.NdbModelView):
     """
     Admin View
     """
-    column_list = [
-        'Cluster', 'Region', 'PreemptiblePct', 'UpContainerPendingRatio',
-        'DownYARNMemAvailePct', 'UpYARNMemAvailPct', 'MaxInstances',
-        'MinInstances'
-    ]
-    column_labels = dict(
-        MinInstances='Min Number of Nodes',
-        MaxInstances='Max number of Nodes',
-        Cluster='Cluster Name',
-        Region='Cluster Region',
-        UpContainerPendingRatio='Container Pending Ratio',
-        UpYARNMemAvailPct='Scale Out % YARNMemoryAvailable',
-        PreemptiblePct='% Preemptible',
-        DownYARNMemAvailePct='Scale In % YARNMemoryAvailable')
+    column_dic = LastUpdatedOrderedDict()
+
+    column_dic['Cluster'] = {
+        'label': 'Cluster Name',
+        'description': 'Google Dataproc Cluster Name'
+    }
+    column_dic['Region'] = {
+        'label': 'Cluster Region',
+        'description': 'Cluster Region'
+    }
+    column_dic['PreemptiblePct'] = {
+        'label': '% Preemptible',
+        'description': 'The ratio of preemptible workers in Dataproc cluster'
+    }
+    column_dic['UpContainerPendingRatio'] = {
+        'label':
+        'Container Pending Ratio',
+        'description':
+        'The ratio of pending containers allocated to trigger scale out event'
+        ' of the cluster'
+    }
+    column_dic['DownYARNMemAvailePct'] = {
+        'label':
+        'Scale In % YARNMemoryAvailable',
+        'description':
+        'The percentage of remaining memory available to YARN to trigger scale'
+        ' down'
+    }
+    column_dic['UpYARNMemAvailPct'] = {
+        'label':
+        'Scale Out % YARNMemoryAvailable',
+        'description':
+        'The percentage of remaining memory available to YARN to trigger'
+        ' scale out'
+    }
+    column_dic['MinInstances'] = {
+        'label':
+        'Min Number of Nodes',
+        'description':
+        'The least number of workers allowed, even if the target is'
+        ' exceeded'
+    }
+    column_dic['MaxInstances'] = {
+        'label':
+        'Max number of Nodes',
+        'description':
+        'The largest number of workers allowed, even if the target is'
+        ' exceeded'
+    }
+
+    column_list = ([key for key in column_dic])
+    column_labels = {
+        key: dict(value).get('label')
+        for key, value in column_dic.items()
+    }
+    column_descriptions = {
+        key: dict(value).get('description')
+        for key, value in column_dic.items()
+    }
     list_template = 'list.html',
     edit_template = 'edit.html',
     create_template = 'create.html',
-    column_descriptions = dict(
-        MinInstances=
-        'The least number of workers the cluster will contain, even if the'
-        ' target is not met',
-        MaxInstances=
-        'The largest number of workers allowed, even if the target is'
-        ' exceeded',
-        Cluster='Google Dataproc Cluster Name',
-        Region='Cluster Region',
-        UpContainerPendingRatio=
-        'The ratio of pending containers allocated to trigger scale out event'
-        ' of the cluster',
-        UpYARNMemAvailPct=
-        'The percentage of remaining memory available to YARN to trigger'
-        ' scale out',
-        PreemptiblePct='The ratio of preemptible workers in Dataproc cluster',
-        DownYARNMemAvailePct=
-        'The percentage of remaining memory available to YARN to trigger scale'
-        ' down')
-    form_args = {
-        'MinInstances': {
-            'label': 'Min Number of Nodes',
-            'validators': [validators.NumberRange(2)]
-        },
-        'MaxInstances': {
-            'label':
-            'Max number of Nodes',
-            'validators': [
-                validators.NumberRange(2),
-                GreaterEqualThan(fieldname='MinInstances')
-            ]
-        },
-        'Cluster': {
-            'label': 'Cluster Name'
-        },
-        'Region': {
-            'label': 'Cluster Region'
-        },
-        'UpContainerPendingRatio': {
-            'label':
-            'Container Pending Ratio',
-            'validators': [
-                validators.NumberRange(0),
-                SmallerEqualThan(fieldname='DownYARNMemAvailePct')
-            ]
-        },
-        'UpYARNMemAvailPct': {
-            'label': 'Scale Out % YARNMemoryAvailable',
-            'validators': [validators.NumberRange(0, 100)]
-        },
-        'PreemptiblePct': {
-            'label': '% Preemptible',
-            'validators': [validators.NumberRange(0, 100)]
-        },
-        'DownYARNMemAvailePct': {
-            'label':
-            'Scale In % YARNMemoryAvailable',
-            'validators': [
-                validators.NumberRange(0, 100),
-                GreaterEqualThan(fieldname='UpYARNMemAvailPct')
-            ]
-        }
-    }
+
+    form_args = column_dic
+    form_args['MinInstances']['validators'] = [validators.NumberRange(2)]
+    form_args['MaxInstances']['validators'] = [
+        validators.NumberRange(2),
+        GreaterEqualThan(fieldname='MinInstances')
+    ]
+    form_args['UpContainerPendingRatio']['validators'] = [
+        validators.NumberRange(0),
+        SmallerEqualThan(fieldname='DownYARNMemAvailePct')
+    ]
+    form_args['UpYARNMemAvailPct']['validators'] = [
+        validators.NumberRange(0, 100)
+    ]
+    form_args['PreemptiblePct']['validators'] = [
+        validators.NumberRange(0, 100)
+    ]
+    form_args['DownYARNMemAvailePct']['validators'] = [
+        validators.NumberRange(0, 100),
+        GreaterEqualThan(fieldname='UpYARNMemAvailPct')
+    ]
